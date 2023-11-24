ed4b9ee2b4f5564e2e1b86cc1474e9631aa4813a
iamkimseohee <kimd5ri@gmail.com>
2023-11-24 17:58:33 +0900

    231124

--- js/questions.js
+++ js/questions.js
@@ -17,5 +17,29 @@ function renderQuestion() {
   choice2El.innerHTML = question.choices[1].text;
   progressValue.style.width = (currentNumber + 1) * 10 + "%";
 }
+function nextQuestion(choiceNumber){
+  if(currentNumber ===questions.length -1){
+    showResultPage()
+    return
+  }
+  const question = questions[currentNumber];
+  mbti = mbti+question.choices[choiceNumber].value
+  // console.log(mbti)
+  currentNumber = currentNumber+1
+  renderQuestion()
 
+
+}
+
+function showResultPage(){
+//  쿼리스트링
+  location.href='/results.html?mbti='+mbti
+}
+ 
+choice1El.addEventListener('click',function(){
+  nextQuestion(0)
+})
+choice2El.addEventListener('click',function(){
+  nextQuestion(1)
+})
 renderQuestion();
